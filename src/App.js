aee8494d6be4cce7ce041151bf98133256e5cbe5
goutam-ullas <goutam.ullas@gmail.com>
2020-09-17 23:41:25 +0200

    2 files modified

--- src/App.js
+++ src/App.js
@@ -596,28 +596,18 @@ class Application extends React.Component {
           >
             &#10005;
           </span>
-          <div className = "researchContainer">
           <img
-            style={{
-              position: "relative",
-              marginTop: window.innerHeight / 10,
-              marginLeft: 50
-            }}
+            style={{marginLeft: 50, marginTop: window.innerHeight/10}}
             src="https://i.imgur.com/oEgq3R8.jpg"
             height={(4 * window.innerHeight) / 5}
             width="auto"
           />
           <img
-            style={{
-              position: "relative",
-              marginTop: window.innerHeight / 10,
-              marginLeft: 50
-            }}
+            style={{marginLeft: 50, marginTop: window.innerHeight/10}}
             src="https://i.imgur.com/Fn7Komh.jpg"
             height={(4 * window.innerHeight) / 5}
             width="auto"
           />
-          </div>
         </div>
       </div>
     );
