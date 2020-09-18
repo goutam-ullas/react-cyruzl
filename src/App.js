7c796b7fde3aebab9660b061d708ec37840cec0d
goutam-ullas <goutam.ullas@gmail.com>
2020-09-18 23:28:05 +0200

    1 file modified

--- src/App.js
+++ src/App.js
@@ -135,6 +135,38 @@ class Application extends React.Component {
         },
         true
       );
+      // Create a popup, but don't add it to the map yet.
+      var popup = new mapboxgl.Popup({
+        closeButton: false,
+        closeOnClick: false
+      });
+
+      this.map.on("mouseenter", "places", function(e) {
+        // Change the cursor style as a UI indicator.
+        this.map.getCanvas().style.cursor = "pointer";
+
+        var coordinates = e.features[0].geometry.coordinates.slice();
+        var description = e.features[0].properties.description;
+
+        // Ensure that if the map is zoomed out such that multiple
+        // copies of the feature are visible, the popup appears
+        // over the copy being pointed to.
+        while (Math.abs(e.lngLat.lng - coordinates[0]) > 180) {
+          coordinates[0] += e.lngLat.lng > coordinates[0] ? 360 : -360;
+        }
+
+        // Populate the popup and set its coordinates
+        // based on the feature found.
+        popup
+          .setLngLat(coordinates)
+          .setHTML(description)
+          .addTo(this.map);
+      });
+
+      this.map.on("mouseleave", "places", function() {
+        this.map.getCanvas().style.cursor = "";
+        popup.remove();
+      });
     });
 
     this.map.on("move", () => {
